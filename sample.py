b11fa7538d582a37ed5c9e2b234bf68a05bce73c
akerue <hsmgn8118akerue@gmail.com>
2014-08-12 00:54:34 +0900

    Change how to define options

--- sample.py
+++ sample.py
@@ -10,6 +10,13 @@ def sample_function(username, password):
     print("Password: {}".format(password))
     return
 
+@frame_wrapper(title="Other frame window", message="他のメッセージも指定可能")
+def sample_function2(username, password):
+    # Do something to log in
+    print("Login Name: {}".format(username))
+    print("Password: {}".format(password))
+    return
+
 def main():
     sample_function()
 
