23e320c2375bceb7f3f944a2f8c7114a53a06223
akerue <hsmgn8118akerue@gmail.com>
2016-11-09 18:27:39 +0900

    Upgrade LoginFrame version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name="LoginFrame",
-        version="0.2.0",
+        version="0.3.0",
         author="akerue",
         author_email="hsmgn8118akerue@gmail.com",
         url="https://github.com/akerue/LoginFrame",
